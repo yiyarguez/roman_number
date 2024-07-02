b0f73fed9240d087cffafca952394a1ec6f933ba
Kary Rodríguez <yiyarguez@gmail.com>
2024-07-01 19:50:50 -0600

    test: del 10 al 90

--- bitacora.md
+++ bitacora.md
@@ -1,10 +1,26 @@
 # Tests
 
-- Crear una función que pade de arabigo a romano (menor de 4000)
+- Crear una función que pase de arabigo a romano (menor de 4000)
 ```
 to_roman(n: int) -> str
 ```
+
+TEST:
+
+Romanos simples:
 * 1 -> 'I'
 * 5 -> 'V'
+
+Romanos del 1 al 9:
+* 2 -> 'II'
+* 3 -> 'III'
+* 4 -> 'IV'
+
+Romanos del 10 al 90:
+* 10 -> 'X'
+* 20 -> 'XX'
+* 30 -> 'XXX'
+* 40 -> 'XL'
+* 50 -> 'L'
 
 
--- roman_funcs.py
+++ roman_funcs.py
@@ -1,3 +1,4 @@
+# Diccionario
 values = {
     1: 'I',
     5: 'V',
@@ -8,44 +9,39 @@ values = {
     1000: 'M'
 }
 
+
+
+
 def to_roman(n: int) -> str:
     if n <= 3:
-        result = n * values[1]
+        result = n * values[1]   # si n = 2, result = 2 * values[1] -> 2 * 'I'
     elif n == 4: 
-        result = values[1] + values[5]
+        result = values[1] + values[5] # 'I' + 'V'
     elif n < 9:
-        result = values[5] + (n - 5) * values[1]
+        result = values[5] + (n - 5) * values[1] # si n = 8, result = 'V' + (8 - 5) * 'I' ->
     elif n == 9:
         result = values[1] + values[10]
+    elif n <= 30:
+        result = int(str(n).replace('0', '')) * values[10] # n = 20, result = 2 * 'X'
+    elif n == 40:
+         result = values[10] + values[50]
+    elif n < 90:
+        result_str = str(n - 50).replace('0', '')
+        result = values[50] + (int(result_str) if result_str else 0) * values[10]
+    elif n == 90:
+        result = values[10] + values[100] #'X' + 'C'
     else:
         result = values[n]
     return result
-        
 
 
 
 
-    
-#   result = values[n]
 
-# Los ifs no son necesarios solo son para mostrar como se realizó antes de implementar
-# el diccionario
 
-"""
-    if n == 1:
-        result = 'I'
-    elif n == 5:
-        result = 'V'
-    elif n == 10:
-        result = 'X'
-    elif n == 50:
-        result = 'L'
-    elif n == 100:
-        result = 'C'
-    elif n == 500:
-        result = 'D'
-    elif n == 1000:
-        result = 'M'
-    
-    return result
-"""
+
+
+
+
+
+
